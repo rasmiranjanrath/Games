d456a0218b3a3414262db12d530de9f42ce9ca1c
rasmiranjan.rath <rasmiranjan.rath@girmiti.com>
2019-08-12 16:41:47 +0530

    enghancements

--- Game.py
+++ Game.py
@@ -1,6 +1,6 @@
 import curses
 from curses import KEY_LEFT, KEY_RIGHT
-from time import sleep
+
 
 
 curses.initscr()
@@ -13,19 +13,23 @@ win.nodelay(1)
 
 key = KEY_RIGHT                                                    # Initializing values
 score = 0
+animation=0
 
-animation = 0
 
 
 
 
 vehicle=[[17,4],[18,5],[18,3],[16,5],[16,3]]
+obj=[[15,5],[14,5],[15,4],[14,4]]
 
-win.addch(vehicle[0][0], vehicle[0][1], '*') 
+
+
+
+win.addch(vehicle[0][0], vehicle[0][1], '*')
 win.addch(vehicle[1][0], vehicle[1][1], '*')
 win.addch(vehicle[2][0], vehicle[2][1], '*')
 win.addch(vehicle[3][0], vehicle[3][1], '*')
-win.addch(vehicle[4][0], vehicle[4][1], '*')   
+win.addch(vehicle[4][0], vehicle[4][1], '*')
 
 while key != 27:                                                   # While Esc key is not pressed
     win.border(0)
@@ -34,54 +38,73 @@ while key != 27:                                                   # While Esc k
     event = win.getch()
     key = key if event == -1 else event
 
+    win.timeout(1000)
+    score+=1
+
     if key == ord(' '):                                            # If SPACE BAR is pressed, wait for another
         key = -1                                                   # one (Pause/Resume)
         while key != ord(' '):
             key = win.getch()
         key = prevKey
         continue
+    obj_old = obj[:]
+
+    if animation == 0:
+        win.addch(obj[0][0], obj[0][1], '#')
+        win.addch(obj[1][0], obj[1][1], '#')
+        win.addch(obj[2][0], obj[2][1], '#')
+        win.addch(obj[3][0], obj[3][1], '#')
+        animation = 1
+    else:
+        win.addch(obj[0][0], obj[0][1], '&')
+        win.addch(obj[1][0], obj[1][1], '@')
+        win.addch(obj[2][0], obj[2][1], '@')
+        win.addch(obj[3][0], obj[3][1], '&')
+        animation = 0
+
+    obj[0][0]+=2
+    obj[1][0]+=2
+    obj[2][0]+=2
+    obj[3][0]+=2
+
+
+
+
+    if obj[2][0]>18 or obj[3][0] >18:
+        obj[0][0] = 1
+        obj[1][0] = 2
+        obj[2][0] = 1
+        obj[3][0] = 2
+
+
 
     if key == KEY_RIGHT:
-    	win.addch(vehicle[0][0], vehicle[0][1], ' ') 
+    	win.addch(vehicle[0][0], vehicle[0][1], ' ')
         win.addch(vehicle[1][0], vehicle[1][1], ' ')
         win.addch(vehicle[2][0], vehicle[2][1], ' ')
         win.addch(vehicle[3][0], vehicle[3][1], ' ')
         win.addch(vehicle[4][0], vehicle[4][1], ' ')
     	vehicle=[[17,15],[18,16],[18,14],[16,16],[16,14]]
-        win.addch(vehicle[0][0], vehicle[0][1], '*') 
+        win.addch(vehicle[0][0], vehicle[0][1], '*')
         win.addch(vehicle[1][0], vehicle[1][1], '*')
         win.addch(vehicle[2][0], vehicle[2][1], '*')
         win.addch(vehicle[3][0], vehicle[3][1], '*')
         win.addch(vehicle[4][0], vehicle[4][1], '*')
 
     if key == KEY_LEFT:
-    	win.addch(vehicle[0][0], vehicle[0][1], ' ') 
+    	win.addch(vehicle[0][0], vehicle[0][1], ' ')
         win.addch(vehicle[1][0], vehicle[1][1], ' ')
         win.addch(vehicle[2][0], vehicle[2][1], ' ')
         win.addch(vehicle[3][0], vehicle[3][1], ' ')
         win.addch(vehicle[4][0], vehicle[4][1], ' ')
     	vehicle=[[17,4],[18,5],[18,3],[16,5],[16,3]]
-        win.addch(vehicle[0][0], vehicle[0][1], '*') 
+        win.addch(vehicle[0][0], vehicle[0][1], '*')
         win.addch(vehicle[1][0], vehicle[1][1], '*')
         win.addch(vehicle[2][0], vehicle[2][1], '*')
         win.addch(vehicle[3][0], vehicle[3][1], '*')
         win.addch(vehicle[4][0], vehicle[4][1], '*')
 
+
+
 curses.endwin()
 
-def enemey():
-	if animation==0:
-		obj=[[11,5],[10,5],[11,4],[10,4]]
-		win.addch(obj[0][0], obj[0][1], '#') 
-        win.addch(obj[1][0], obj[1][1], '#')
-        win.addch(obj[2][0], obj[2][1], '#')
-        win.addch(obj[3][0], obj[3][1], '#')
-        animation=1
-    else:
-     	obj=[[11,5],[10,5],[11,4],[10,4]]
-        win.addch(obj[0][0], obj[0][1], '@') 
-        win.addch(obj[1][0], obj[1][1], '&')
-        win.addch(obj[2][0], obj[2][1], '%')
-        win.addch(obj[3][0], obj[3][1], '!')
-        animation=0
-    sleep(1)
