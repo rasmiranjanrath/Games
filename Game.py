16277853a668f92debbfca4ad68e282e1a5d6143
rasmiranjan.rath <rasmiranjan.rath@girmiti.com>
2019-08-17 14:20:14 +0530

    2 side vehicle

--- Game.py
+++ Game.py
@@ -1,5 +1,6 @@
 import curses
 from curses import KEY_LEFT, KEY_RIGHT
+import random
 
 
 
@@ -17,10 +18,13 @@ animation=0
 collision=0
 v_position=1
 
+object_position=0
+
 
 vehicle=[[17,4],[18,5],[18,3],[16,5],[16,3]]
 vehicle1=[[17,15],[18,16],[18,14],[16,16],[16,14]]
 obj=[[15,5],[14,5],[15,4],[14,4]]
+obj_right=[[15,15],[14,16],[15,15],[14,16]]
 
 vehicle_x=[vehicle[0][0],vehicle[1][0],vehicle[2][0],vehicle[3][0]]
 vehicle_y=[vehicle[0][1],vehicle[1][1],vehicle[2][1],vehicle[3][1]]
@@ -62,7 +66,7 @@ while key != 27:                                                   # While Esc k
     win.addch(obj[1][0], obj[1][1], ' ')
     win.addch(obj[2][0], obj[2][1], ' ')
     win.addch(obj[3][0], obj[3][1], ' ')
-
+    
 
     #calculate colision for left position
     if obj[0][0] == vehicle [0][0] and v_position !=2:
@@ -78,6 +82,7 @@ while key != 27:                                                   # While Esc k
 
 
     if obj[2][0]>18 or obj[3][0] >18:
+        object_position=random.randint(0,2)
         obj[0][0] = 1
         obj[1][0] = 2
         obj[2][0] = 1
@@ -111,22 +116,37 @@ while key != 27:                                                   # While Esc k
         win.addch(vehicle[4][0], vehicle[4][1], '*')
         v_position=1
 
-    if True:
-        old_obj = obj
-        if animation == 0:
-            win.addch(old_obj[0][0], old_obj[0][1], '#')
-            win.addch(old_obj[1][0], old_obj[1][1], '#')
-            win.addch(old_obj[2][0], old_obj[2][1], '#')
-            win.addch(old_obj[3][0], old_obj[3][1], '#')
-            animation = 1
-        else:
-            win.addch(old_obj[0][0], old_obj[0][1], '@')
-            win.addch(old_obj[1][0], old_obj[1][1], '&')
-            win.addch(old_obj[2][0], old_obj[2][1], '@')
-            win.addch(old_obj[3][0], old_obj[3][1], '&')
-            animation = 0
-    del old_obj
-
-
+    if True:    
+        if object_position==1:
+            old_obj_left = obj
+            if animation == 0:
+                win.addch(old_obj_left[0][0], old_obj_left[0][1], '#')
+                win.addch(old_obj_left[1][0], old_obj_left[1][1], '#')
+                win.addch(old_obj_left[2][0], old_obj_left[2][1], '#')
+                win.addch(old_obj_left[3][0], old_obj_left[3][1], '#')
+                animation = 1
+            else:
+                win.addch(old_obj_left[0][0], old_obj_left[0][1], '@')
+                win.addch(old_obj_left[1][0], old_obj_left[1][1], '&')
+                win.addch(old_obj_left[2][0], old_obj_left[2][1], '@')
+                win.addch(old_obj_left[3][0], old_obj_left[3][1], '&')
+                animation = 0
+            del old_obj_left
+        if object_position==2:
+            obj_right=[[obj[0][0],15],[obj[1][0],16],[obj[2][0],15],[obj[3][0],16]]
+            if animation == 0:
+                win.addch(obj_right[0][0], obj_right[0][1], '#')
+                win.addch(obj_right[1][0], obj_right[1][1], '#')
+                win.addch(obj_right[2][0], obj_right[2][1], '#')
+                win.addch(obj_right[3][0], obj_right[3][1], '#')
+                animation = 1
+            else:
+                win.addch(obj_right[0][0], obj_right[0][1], '@')
+                win.addch(obj_right[1][0], obj_right[1][1], '&')
+                win.addch(obj_right[2][0], obj_right[2][1], '@')
+                win.addch(obj_right[3][0], obj_right[3][1], '&')
+                animation = 0
+            del obj_right
+            
 curses.endwin()
 
